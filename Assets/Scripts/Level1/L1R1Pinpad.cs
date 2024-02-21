4cc3e01bd2c7c594c0984bd4f10372573b86eb42
Michael Francis <58670911+mfrancis33@users.noreply.github.com>
2024-02-20 19:45:33 -0500

    Stopped infinite door and scripted lights

--- Assets/Scripts/Level1/L1R1Pinpad.cs
+++ Assets/Scripts/Level1/L1R1Pinpad.cs
@@ -6,13 +6,37 @@ public class L1R1Pinpad : MonoBehaviour
 {
 
     [SerializeField] GameObject door;
+    [SerializeField] GameObject lightSuccessObj;
+    [SerializeField] GameObject lightFailObj;
+
+    private Animator doorAnimator;
+    private Light lightSuccess;
+    private Light lightFail;
 
     void Start()
     {
+        doorAnimator = door.GetComponent<Animator>();
+        lightSuccess = lightSuccessObj.GetComponent<Light>();
+        lightFail = lightFailObj.GetComponent<Light>();
+
+        lightSuccess.enabled = false;
+        lightFail.enabled = false;
+
         Pinpad p = GetComponent<Pinpad>();
         
         p.OnSuccess += () => {
-            Debug.Log("WOOHOO");
+            lightSuccess.enabled = true;
+            Debug.Log("Correct :)");
+        };
+        p.OnFail += () =>
+        {
+            lightFail.enabled = true;
+            Debug.Log("Incorrect :(");
+        };
+        p.OnKeyPress += (_, _) =>
+        {
+            lightFail.enabled = false;
+            Debug.Log("Tap!");
         };
     }
 }
