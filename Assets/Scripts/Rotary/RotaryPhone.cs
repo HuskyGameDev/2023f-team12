4416bee5c663b3dbdb2222714a5034924ecc5f0f
Michael Francis <58670911+mfrancis33@users.noreply.github.com>
2024-03-26 19:49:29 -0400

    Removed testing remains

--- Assets/Scripts/Rotary/RotaryPhone.cs
+++ Assets/Scripts/Rotary/RotaryPhone.cs
@@ -5,37 +5,21 @@ using UnityEngine;
 public class RotaryPhone : MonoBehaviour
 {
     [SerializeField] public string[] PhoneNumbers = new string[0];
-    [SerializeField] public GameObject DoorToOpen = null;
-    [SerializeField] public float DoorOpenTime = 5f;
-
-    [SerializeField] GameObject TestObjectDeleteMePleaseItsOnTheTrashCan = null;
 
     internal string CorrectNumber;
     internal List<string> EnteredNumber = new();
     internal bool Correct = false;
 
-    private float currTime = 0f;
-    private Vector3 initialPos;
-
     void Start()
     {
         CorrectNumber = PhoneNumbers[Random.Range(0, PhoneNumbers.Length)];
-        initialPos = DoorToOpen.transform.position;
-
-
-        // TESTING ONLY
-        TestObjectDeleteMePleaseItsOnTheTrashCan.GetComponent<Pinpad>().OnSuccess += () => EnteredNumber.Add("3");
     }
 
     void Update()
     {
         if (Correct)
         {
-            if (currTime < DoorOpenTime)
-            {
-                currTime += Time.deltaTime;
-                DoorToOpen.transform.position = Vector3.Lerp(initialPos, initialPos - new Vector3(5, 0, 0), currTime / DoorOpenTime);
-            }
+            // do stuff
         }
         else
         {
