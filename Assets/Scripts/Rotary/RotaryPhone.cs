2dd506c04bc8d94e118e184cefb35d18b358f6c6
Michael Francis <58670911+mfrancis33@users.noreply.github.com>
2024-03-26 20:32:48 -0400

    Rotary phone does stuff, pinpad has a delay

--- Assets/Scripts/Rotary/RotaryPhone.cs
+++ Assets/Scripts/Rotary/RotaryPhone.cs
@@ -1,22 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RotaryPhone : MonoBehaviour
 {
+    [Header("Puzzle")]
     [SerializeField] public string[] PhoneNumbers = new string[0];
 
-    internal string CorrectNumber;
+    [Header("Controls")]
+    [SerializeField] public GameObject One;
+    [SerializeField] public GameObject Two;
+    [SerializeField] public GameObject Three;
+    [SerializeField] public GameObject Four;
+    [SerializeField] public GameObject Five;
+    [SerializeField] public GameObject Six;
+    [SerializeField] public GameObject Seven;
+    [SerializeField] public GameObject Eight;
+    [SerializeField] public GameObject Nine;
+    [SerializeField] public GameObject Zero;
+    [SerializeField] public GameObject Pound;
+    [SerializeField] public GameObject Asterisk;
+
+    [Header("Debug")]
+    [SerializeField] public string CorrectNumber;
     internal List<string> EnteredNumber = new();
     internal bool Correct = false;
+    internal bool Accepting = true;
 
     void Start()
     {
-        CorrectNumber = PhoneNumbers[Random.Range(0, PhoneNumbers.Length)];
+        if (PhoneNumbers.Length > 0)
+        {
+            CorrectNumber = PhoneNumbers[Random.Range(0, PhoneNumbers.Length)];
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                CorrectNumber += Random.Range(0, 10).ToString();
+            }
+        }
+
+        // Deal with the controls and stuff
+        if (Util.TryGetComponent<Interactable>(One,   out var inter1)) inter1.OnInteract += (_, _) => HandleButton("1");
+        if (Util.TryGetComponent<Interactable>(Two,   out var inter2)) inter2.OnInteract += (_, _) => HandleButton("2");
+        if (Util.TryGetComponent<Interactable>(Three, out var inter3)) inter3.OnInteract += (_, _) => HandleButton("3");
+        if (Util.TryGetComponent<Interactable>(Four,  out var inter4)) inter4.OnInteract += (_, _) => HandleButton("4");
+        if (Util.TryGetComponent<Interactable>(Five,  out var inter5)) inter5.OnInteract += (_, _) => HandleButton("5");
+        if (Util.TryGetComponent<Interactable>(Six,   out var inter6)) inter6.OnInteract += (_, _) => HandleButton("6");
+        if (Util.TryGetComponent<Interactable>(Seven, out var inter7)) inter7.OnInteract += (_, _) => HandleButton("7");
+        if (Util.TryGetComponent<Interactable>(Eight, out var inter8)) inter8.OnInteract += (_, _) => HandleButton("8");
+        if (Util.TryGetComponent<Interactable>(Nine,  out var inter9)) inter9.OnInteract += (_, _) => HandleButton("9");
+        if (Util.TryGetComponent<Interactable>(Zero,  out var inter0)) inter0.OnInteract += (_, _) => HandleButton("0");
     }
 
     void Update()
     {
+        Accepting = true; // TODO: REMOVE
+
         if (Correct)
         {
             // do stuff
@@ -38,13 +81,30 @@ public class RotaryPhone : MonoBehaviour
                 if (correct)
                 {
                     Correct = true;
+                    Accepting = false;
+                    OnSuccess?.Invoke();
+                    Debug.Log("Phone number correct");
                 }
                 else
                 {
                     EnteredNumber.Clear();
+                    OnFail?.Invoke();
+                    Debug.Log("Phone number incorrect");
                 }
             }
         }
-
     }
+
+    internal void HandleButton(string num)
+    {
+        Debug.Log(num);
+        if (Accepting)
+        {
+            Accepting = false;
+            EnteredNumber.Add(num);
+        }
+    }
+
+    public event Action OnSuccess;
+    public event Action OnFail;
 }
