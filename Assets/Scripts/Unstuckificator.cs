8a04d1686b3950e94e2daaf6e148ab98e88a62fa
Michael Francis <58670911+mfrancis33@users.noreply.github.com>
2024-04-16 22:21:43 -0400

    Painting room door stuck fixed

--- Assets/Scripts/Unstuckificator.cs
+++ Assets/Scripts/Unstuckificator.cs
@@ -4,44 +4,30 @@ using UnityEngine;
 
 public class Unstuckificator : MonoBehaviour
 {
-    // [SerializeField] GameObject trigger;
-
     [SerializeField] bool enabled = false;
-    private Vector2 SafetyPos;
+    private Vector3 SafetyPos;
 
     void Start()
     {
-        // Trigger = transform.Find("Trigger").GetComponent<BoxCollider>();
-        // GetComponent<BoxCollider>().enabled = false;
         SafetyPos = transform.Find("Safety").transform.position;
+        Debug.Log(SafetyPos.x + "," + SafetyPos.y + "," + SafetyPos.z);
     }
 
     void Update()
     {
         if (enabled)
         {
-            // https://docs.unity3d.com/ScriptReference/Physics.OverlapBox.html
-            Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, 0);
-            // Check when there is a new collider coming into contact with the box
-            for (int i = 0; i < hitColliders.Length; i++)
+            var myPos = transform.position;
+            var playerPos = Global.Player.transform.position;
+            if (Vector2.Distance(new Vector2(myPos.x, myPos.z), new Vector2(playerPos.x, playerPos.z)) < 1f)
             {
-                var other = hitColliders[i];
-                Debug.Log(other);
-                if (enabled && other.tag == "Player")
-                {
-                    Global.Player.transform.position = SafetyPos;
-                }
+                Global.Player.transform.position = SafetyPos;
             }
         }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
     }
 
     public void EnableTrigger()
     {
         enabled = true;
-        // GetComponent<BoxCollider>().enabled = true;
     }
 }
