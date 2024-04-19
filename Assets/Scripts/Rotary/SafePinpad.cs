04b7b8958da2e87ef4127ab343fc530f6f9522d6
Michael Francis <58670911+mfrancis33@users.noreply.github.com>
2024-04-18 20:58:57 -0400

    Safe pinpad sounds

--- Assets/Scripts/Rotary/SafePinpad.cs
+++ Assets/Scripts/Rotary/SafePinpad.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class SafePinpad : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private AudioSource pinClick;
+    private AudioSource resetClick;
+    private AudioSource openClick;
+
     void Start()
     {
+        // Sound
+        var audioSources = GetComponents<AudioSource>(); // populated as ordered in inspector
+        pinClick = audioSources[0];
+        resetClick = audioSources[1];
+        openClick = audioSources[2];
+
         // Pinpad and events
         Pinpad p = GetComponent<Pinpad>();
 
+        p.OnKeyPress += (_, _) =>
+        {
+            pinClick.Play();
+        };
+        p.OnReset += () => resetClick.Play();
+        p.OnFail += () => resetClick.Play();
         p.OnSuccess += () =>
         {
+            openClick.Play();
             GetComponent<Animator>().Play("Base Layer.OpenSmall");
         };
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
